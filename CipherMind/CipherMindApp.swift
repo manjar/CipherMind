cb567d51b8380b46452d3565cd1546012781e184
Eli Manjarrez <manjarrez@apple.com>
2020-11-21 10:24:34 -0800

    Initial Commit

---
+++ CipherMind/CipherMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  CipherMindApp.swift
+//  CipherMind
+//
+//  Created by Eli Manjarrez on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct CipherMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
